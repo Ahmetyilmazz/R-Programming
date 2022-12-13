cde98e16973804e06ee72493151b1d60c4cd7f47
Ahmet Yılmaz <ahmeet.ylmzzz@gmail.com>
2022-12-13 18:45:23 +0300

    LDLt_Factorization
    LDLt_Factorization with R

---
+++ LDLt_Factorization.R
@@ -0,0 +1,32 @@
+
+
+# Programmer : AHMET YILMAZ 
+# Matematik - Bilgisayar / 4
+
+
+A <- A<- matrix(c(4,-1,1,
+                  -1,4.25,2.75,
+                  1,2.75,3.5) ,byrow=T, nrow=3)
+
+LDLt <- function(A){
+  
+  n <- nrow(A)
+  L <- matrix(c(0),n,n)
+  
+  # Step - 1
+  for(i in 1:n){
+    # Step - 2
+    for (j in 1:(i-1)){
+      V[j] <- L[i,j] * D(j)
+    }
+    
+    # Step - 3
+    D[i] <- A[i,i] - sum(L[i,1:(i-1)] * V[j])
+    
+    # Step - 4
+    for (j in (i+1):n){
+      L[j,i] <- ((A[j,i]) - sum()) / D[i]
+    }
+  }
+  
+}
